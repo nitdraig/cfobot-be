b37975d793a0734055404c8b9b4dedea53f2cdcb
Agustín Avellaneda <agustin2051@gmail.com>
2023-08-06 21:33:12 -0300

    Act3

--- models/User.js
+++ models/User.js
@@ -1,11 +1,38 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const interaccionSchema = new mongoose.Schema({
-  role: String,
-  content: String,
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   timestamp: { type: Date, default: Date.now },
 });
 
-const Interaccion = mongoose.model("Interaccion", interaccionSchema);
+userSchema.pre("save", function (name) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(document.password, saltRounds, (error, hashedPassword) => {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
 
-module.exports = Interaccion;
+userSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, function (err, same) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
+    }
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,49 +1,10 @@
-// const express = require("express");
-// const cors = require("cors"); // Asegúrate de que cors se importe aquí
-// const app = express();
-// const archivoBD = require("./conection");
-// const PORT = process.env.PORT || 5000;
-
-// //conexion a mongo
-// const mongoose = require("mongoose");
-// const objetobd = mongoose.connection;
-// mongoose.connect("mongodb://127.0.0.1:27017/cfo-db", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// objetobd.on("connected", () => {
-//   console.log("conexion correcta a mongo");
-// });
-// objetobd.on("error", () => {
-//   console.log("conexion incorrecta a mongo");
-// });
-
-// const interaccionRoutes = require("./routes/interaccionRoutes"); // Importa las rutas
-
-// app.use(express.json());
-
-// app.use("/interacciones", interaccionRoutes); // Usa las rutas
-
-// // Configura el middleware de CORS antes de definir las rutas
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//   })
-// );
-
-// app.use(express.json());
-
-// const botRoutes = require("./routes/botRoutes");
-// app.use("/bot", botRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Servidor escuchando en el puerto ${PORT}`);
-// });
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bcrypt = require("bcrypt");
 const archivoBD = require("./conection");
 const PORT = process.env.PORT || 5000;
+const User = require("./models/User"); // Importa el modelo de manera correcta
 
 // Conexion a MongoDB
 const mongoose = require("mongoose");
@@ -58,6 +19,41 @@ objetobd.on("connected", () => {
 objetobd.on("error", () => {
   console.log("Conexion incorrecta a MongoDB");
 });
+
+app.use(express.json());
+
+// app.post("/register", async (req, res) => {
+//   const { username, password, email } = req.body;
+//   try {
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const newUser = new User({ username, password: hashedPassword, email });
+//     await newUser.save();
+//     res.status(200).send("usuario registrado");
+//   } catch (error) {
+//     res.status(500).send("error al registrar usuario");
+//   }
+// });
+
+// app.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
+//   User.findOne({ username }, (err, user) => {
+//     if (err) {
+//       res.status(500).send("error al validar usuario");
+//     } else if (!user) {
+//       res.status(500).send("usuario inexistente");
+//     } else {
+//       user.isCorrectPassword(password, (err, result) => {
+//         if (err) {
+//           res.status(500).send("error al autenticar");
+//         } else if (result) {
+//           res.status(200).send("exito al autenticar");
+//         } else {
+//           res.status(500).send("usuario y/o contraseña incorrecta");
+//         }
+//       });
+//     }
+//   });
+// });
 
 // Configura el middleware de CORS antes de definir las rutas
 app.use(
